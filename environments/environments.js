30daaef4882c3f68b84bed1e3c32e6d32ce75cb7
Felipe Miguel Dos Santos <felipe.msantos@gruposaga.com.br>
2020-05-27 10:39:16 -0300

    Ajuste no .env

--- environments/environments.js
+++ environments/environments.js
@@ -1,2 +1,2 @@
-export const LINK_EXTERNO = "https://sisimlserver.herokuapp.com/"
-export const TIME = 3600000
+exports const LINK_EXTERNO = "https://sisimlserver.herokuapp.com/"
+exports const TIME = 3600000
