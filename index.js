871abb4560cfc8c599a1ea1339922b9af04a0701
Felipe Miguel Dos Santos <felipe.msantos@gruposaga.com.br>
2020-05-27 09:07:10 -0300

    Ajuste no .env

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 const express = require("express")
 const http = require("http")
 const axios = require("axios")
+require("dotenv/config")
 
 const server = http.createServer(express)
 
 const port = process.env.PORT || 6000
 
-//const linkServidorExterno = "http://localhost:5000" //https://sisimlserver.herokuapp.com/
-const linkServidorExterno = "https://sisimlserver.herokuapp.com/"
-const time = 3600000 // 1 Hora
-
 setInterval(async ()=> {
-    await axios.get(`${linkServidorExterno}/atualizador_refresh_token`).then(response => {
+    await axios.get(`${process.env.LINK_EXTERNO}/atualizador_refresh_token`).then(response => {
         console.log("\n")
-        console.log("Data e hora: "+new Date())
+        console.log(process.env.DATA_HORA+" "+new Date())
         console.log(response.data)
         console.log("\n")
     }).catch(err => console.log(err))
-}, time)
+}, Number(process.env.TIME))
 
 server.listen(port, () => {
     console.log("\n")
